d3ebc89a37994ad5cca8065df67b3c9a9044757d
ux-ui-pro <maxim@ux-ui.pro>
2021-12-06 17:26:50 +0300

    changes for the GitHub Pages

--- src/js/awards.js
+++ src/js/awards.js
@@ -1,6 +1,6 @@
 function svg() {
 
-    let file = "/img/svg/icons.svg",
+    let file = "img/svg/icons.svg",
         revision = 123;
 
     if( !document.createElementNS || !document.createElementNS( "http://www.w3.org/2000/svg", "svg" ).createSVGRect ) {
